65c81cd67f1a0a641c53199fc2396bdb461d02cd
mohammad ibrahim <imh774077@gmail.com>
2024-01-15 23:45:03 +0500

    dashboard implement

---
+++ ShowroomManagement/ShowroomManagement/wwwroot/ProjectFiles/Department/index.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    loadData()
+});
+//----------Load Data Fromn API------------------
+
+function loadData() {
+    $.ajax({
+        url: APIURLS.department_DetDepartments,
+        type: 'GET',
+        data: null,
+
+        success: function (resp) {
+            var data = JSON.parse(resp)
+
+            for (let item of data.Response) {
+                $('#example1 tbody').append(`
+                <tr>
+                <td>${item.Name}</td>  
+                <td>${item.Description}</td>
+                </tr>
+
+                `);
+
+            }
+
+
+
+         }
+            
+        }
+
+    }),
+}
---
+++ ShowroomManagement/ShowroomManagement/wwwroot/ProjectFiles/Utilities/ApiUrls.js
@@ -0,0 +1,7 @@
+﻿let main_api = 'http://localhost:5181/api';
+
+let APIURLS = {
+
+    //---------Department--------------x    
+    department_DetDepartments: main_api +'/Department/GetDepartments'
+} 
